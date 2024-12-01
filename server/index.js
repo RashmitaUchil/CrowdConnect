cbda5aa5060166dc2218ef4d9946a689307e97ce
Zaid Khan <zaiddkhhan@gmail.com>
2024-12-01 12:13:35 +0530

    webhook fixed

--- server/index.js
+++ server/index.js
@@ -17,30 +17,6 @@ const donationRouter = require("./routers/donationRouter");
 const authRouter = require("./routers/authRouter");
 
 
-// middleware setup
-app.use(cookieParser());
-app.use(express.json());
-app.use(
-  cors({
-    credentials: true,
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
-
-
-
-app.use("/api/user", userRouter);
-app.use("/api/campaign", campaignRouter);
-app.use("/api/payment", paymentRouter);
-app.use("/api/donation", donationRouter);
-app.use("/auth", authRouter);
-app.post("/create-checkout-session", create_checkout_session)
-
-const stripe = require('stripe')('sk_test_51MaL6pSEfjueS3xIMQ6M4e5HfDZlKloQTqIFkQFBrmI3c9sC3xgsZrVe9sh95LCqmQMG7YGFGAIAbfqFhAS0A1Ur00ttVvB0gZ');
-const endpointSecret = "whsec_8f18bd7d42dddb1b36c647eb13f24bef6a1748a94aca69682ee336f32bf0c927";
 
 app.post('/webhook', express.raw({type: 'application/json'}),async (request, response) => {
 
@@ -74,6 +50,32 @@ app.post('/webhook', express.raw({type: 'application/json'}),async (request, res
   // Return a 200 response to acknowledge receipt of the event
   response.send();
 });
+
+
+// middleware setup
+app.use(cookieParser());
+app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+
+
+
+app.use("/api/user", userRouter);
+app.use("/api/campaign", campaignRouter);
+app.use("/api/payment", paymentRouter);
+app.use("/api/donation", donationRouter);
+app.use("/auth", authRouter);
+app.post("/create-checkout-session", create_checkout_session)
+
+const stripe = require('stripe')('sk_test_51MaL6pSEfjueS3xIMQ6M4e5HfDZlKloQTqIFkQFBrmI3c9sC3xgsZrVe9sh95LCqmQMG7YGFGAIAbfqFhAS0A1Ur00ttVvB0gZ');
+const endpointSecret = "whsec_8f18bd7d42dddb1b36c647eb13f24bef6a1748a94aca69682ee336f32bf0c927";
 
 
 // database connection
